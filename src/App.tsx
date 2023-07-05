313ee037afce57bf99119285b0f90faf7ce9cfd3
ken-1200 <kensuke19971022@gmail.com>
2023-07-05 18:59:18 +0900

    Update import/order

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,23 @@ function App() {
 
   return (
     <>
+      <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-8">
+        <div>
+          <img src="/mountains-1.jpg" alt="" loading="lazy" />
+        </div>
+        <div className="col-start-3">
+          <img src="/mountains-2.jpg" alt="" loading="lazy" />
+        </div>
+        <div>
+          <img src="/mountains-3.jpg" alt="" loading="lazy" />
+        </div>
+        <div>
+          <img src="/mountains-4.jpg" alt="" loading="lazy" />
+        </div>
+        <div className="row-start-1 col-start-2 col-span-2">
+          <img src="/mountains-5.jpg" alt="" loading="lazy" />
+        </div>
+      </div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
