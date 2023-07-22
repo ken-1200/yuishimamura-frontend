f98b5584daad7511f2a57fc8be8d77054c4bfc10
ken-1200 <kensuke19971022@gmail.com>
2023-07-23 00:41:37 +0900

    add ip white list & basic authorization

--- terraform/src/func_spa.js
+++ terraform/src/func_spa.js
@@ -1,17 +1,36 @@
 function handler(event) {
   var request = event.request;
+  var clientIP = event.viewer.ip;
+  var headers = request.headers;
   var uri = request.uri;
 
-  // Check whether the URI is missing a file name.
-  if (uri.endsWith('/')) {
-    request.uri += 'index.html';
-    return request;
-  }
-  // Check whether the URI is missing a file extension.
-  else if (!uri.includes('.')) {
-    request.uri += '/index.html';
-    return request;
+  if (uri.endsWith('/upload_images') || uri.endsWith('/edit_images')) {
+    // アクセス許可するIPを設定
+    var IP_WHITE_LIST = ['126.51.205.12'];
+    // クライアントIPが、アクセス許可するIPに含まれていればtrueを返す
+    var isPermittedIp = IP_WHITE_LIST.includes(clientIP);
+
+    // 許可していないIPの場合
+    if (isPermittedIp === false) {
+      // falseの場合はViewerに対してレスポンスを返す
+      return {
+        statusCode: 403,
+        statusDescription: 'Forbidden',
+      };
+    }
+
+    // echo -n user:pass | base64
+    var authString = 'Basic eXVpc2hpbWFtdXJhOkEza04wV01melRXRzJrdlM=';
+
+    if (typeof headers.authorization === 'undefined' || headers.authorization.value !== authString) {
+      return {
+        statusCode: 401,
+        statusDescription: 'Unauthorized',
+        headers: { 'www-authenticate': { value: 'Basic' } },
+      };
+    }
   }
 
   return request;
 }
+
