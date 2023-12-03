f2a0aa6c5db7e7bee2d620bcb29bced32835eebb
ken-1200 <kensuke19971022@gmail.com>
2023-12-03 22:30:00 +0900

    update z-index

--- src/components/atoms/ImagePreview/index.tsx
+++ src/components/atoms/ImagePreview/index.tsx
@@ -49,7 +49,7 @@ const ImagePreview = ({ src, alt }: Props) => {
             width: 100%;
             height: 100%;
             background-color: rgba(0, 0, 0, 0.8);
-            z-index: 1;
+            z-index: 1000;
           `}
         >
           <XMarkIcon
