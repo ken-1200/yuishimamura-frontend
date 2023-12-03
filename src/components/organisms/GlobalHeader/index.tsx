bc61257779867f52e4d92c93e4257a43af631aab
ken-1200 <kensuke19971022@gmail.com>
2023-12-03 22:31:39 +0900

    update GlobalHeader

--- src/components/organisms/GlobalHeader/index.tsx
+++ src/components/organisms/GlobalHeader/index.tsx
@@ -25,7 +25,7 @@ const GlobalHeader = () => {
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             onClick={() => setMobileMenuOpen(true)}
           >
-            <Bars3Icon className="h-8 w-8" aria-hidden="true" />
+            <Bars3Icon className="h-10 w-10" aria-hidden="true" />
           </button>
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -91,6 +91,13 @@ const GlobalHeader = () => {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
+                <Link
+                  to="/"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-2xl font-semibold leading-9 text-slate-700 hover:bg-slate-100 hover:text-slate-900"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  ホーム
+                </Link>
                 <Link
                   to="/gallery"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
