4073f0c8df7177d0fd3bd21f3c3bbf97c99e532d
ken-1200 <kensuke19971022@gmail.com>
2024-03-08 15:03:00 +0900

    Update logo image source

--- src/components/organisms/TopPage/index.tsx
+++ src/components/organisms/TopPage/index.tsx
@@ -14,7 +14,7 @@ const TopPage = () => {
         ogDescription={HEAD_DESCRIPTION_TOP}
       />
       <div className="absolute bottom-0 right-0 bg-white py-24 sm:py-32">
-        <img className="w-2/5 sm:w-1/4 ml-auto" src="/images/og_image.webp" alt="logo" />
+        <img className="w-2/5 sm:w-1/4 ml-auto" src="/images/profile_logo.webp" alt="logo" />
       </div>
     </>
   );
